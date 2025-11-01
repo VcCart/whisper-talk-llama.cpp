8082133ca83789c2a62c4651e68af0aa9f2ec1e1
VcCart <vasya@pupkin.com>
2025-11-01 10:35:36 +0700

    Добавляем vad_simple_int в examples\common.cpp // возвращает: 0:речи нет, 1:начата речь, 2:закончена речь Добавляем vad_simple_int в examples\common.h // Базовое обнаружение речевой активности (VAD) с использованием порога адаптации к энергии звука

--- examples/common.h
+++ examples/common.h
@@ -240,14 +240,14 @@ void high_pass_filter(
         float sample_rate);
 
 // Basic voice activity detection (VAD) using audio energy adaptive threshold
-bool vad_simple(
+int vad_simple_int(
         std::vector<float> & pcmf32,
         int   sample_rate,
         int   last_ms,
         float vad_thold,
         float freq_thold,
-        bool  verbose);
-
+        bool  verbose,
+        float  vad_start_thold);				   
 // compute similarity between two strings using Levenshtein distance
 float similarity(const std::string & s0, const std::string & s1);
 
